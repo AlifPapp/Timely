a26317442e0c5ead33dc5830dee904a2ecfedfdf
Zseni <83803826+Zseni-Verified@users.noreply.github.com>
2021-06-07 11:10:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pymongo import MongoClient
 from datetime import datetime
 
 
-defaultprefix = "test"
+defaultprefix = "t"
 #####################################################################################################################################
 ######################################################### GET SERVER PREFIX #########################################################
 #####################################################################################################################################
@@ -58,9 +58,9 @@ client.Green = int("3BA55C" , 16)
 client.Red = int("D72D42" , 16)
 client.Blue = int("7289DA" , 16)
 
-MongoClientLink = open("MongoClient.txt","r").readline()
-cluster = MongoClient(MongoClientLink.strip(), ssl_cert_reqs=ssl.CERT_NONE)
-#cluster = MongoClient(str(os.environ.get('MONGO_LINK')), ssl_cert_reqs=ssl.CERT_NONE)
+#MongoClientLink = open("MongoClient.txt","r").readline()
+#cluster = MongoClient(MongoClientLink.strip(), ssl_cert_reqs=ssl.CERT_NONE)
+cluster = MongoClient(str(os.environ.get('MONGO_LINK')), ssl_cert_reqs=ssl.CERT_NONE)
 client.currencydata = cluster["Currency"]["Main"]
 client.serverprefixcluster = cluster["Settings"]["ServerPrefix"]
 
@@ -166,6 +166,6 @@ async def changeprefix(ctx, newprefix: str="None"):
         await ctx.reply(embed = em)
 
 #####################################################################################################################################
-token = open("token.txt","r").readline()
-client.run(token.strip())
-#client.run(str(os.environ.get('BOT_TOKEN')))
+#token = open("token.txt","r").readline()
+#client.run(token.strip())
+client.run(str(os.environ.get('BOT_TOKEN')))
