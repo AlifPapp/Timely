2c727afcd1e2bacb03be253df9932fbb7762dfa0
Zseni <83803826+Zseni-Verified@users.noreply.github.com>
2021-09-30 22:37:54 +1000

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from .functions import basic_embed
+
 class other(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -13,13 +15,15 @@ class other(commands.Cog):
             if ctx.guild is not None and not ctx.author.bot: return True
         except: pass
     
-    # tdevpurge <ammount>
-    @commands.command()
-    async def devpurge(self, ctx, l: int = 50):
-        if ctx.guild is not None:
+    # tpurge <ammount>
+    @commands.command(aliases=['clear'])
+    async def purge(self, ctx, amt: int = 50):
+        if ctx.author.guild_permissions.manage_messages:
             await ctx.message.delete()
-            c = await ctx.channel.purge(limit=l)
+            c = await ctx.channel.purge(limit=amt)
             await ctx.send(f"Cleared {len(c)} messages", delete_after=3)
+        else:
+            await ctx.reply(embed = await basic_embed(f"", f"<:danger:848526668024250408> You are missing the permission `manage_messages`.",self.client.Red,""))
 
 #####################################################################################################################################
 def setup(client):
