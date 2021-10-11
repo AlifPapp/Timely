35395aa26b9b57ea41d774f6cb1cff21da58067b
Zseni <83803826+Zseni-Verified@users.noreply.github.com>
2021-10-12 09:43:55 +1000

    Add files via upload

---
+++ cogs/other.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+
+from .functions import basic_embed
+
+class other(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("other.py Loaded!")
+    
+    async def cog_check(self, ctx):
+        try: 
+            if ctx.guild is not None and not ctx.author.bot: return True
+        except: pass
+    
+    # tpurge <ammount>
+    @commands.command(aliases=['clear'])
+    async def purge(self, ctx, amt: int = 50):
+        if ctx.author.guild_permissions.manage_messages:
+            await ctx.message.delete()
+            c = await ctx.channel.purge(limit=amt)
+            await ctx.send(f"Cleared {len(c)} messages", delete_after=3)
+        else:
+            await ctx.reply(embed = await basic_embed(f"", f"{self.client.Emojis['danger']} You are missing the permission `manage_messages`.",self.client.Red,""))
+
+#####################################################################################################################################
+def setup(client):
+    client.add_cog(other(client))
